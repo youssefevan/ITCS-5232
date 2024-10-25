11d172af3301f626836cce3f242eec14944b4593
Evan Youssef <evanyoussef8@gmail.com>
2024-10-25 13:39:26 -0400

    create astar grid

--- itcs-5232/scripts/tiles.gd
+++ itcs-5232/scripts/tiles.gd
@@ -9,6 +9,7 @@ var rng = RandomNumberGenerator.new()
 
 var used_cells = []
 var borders
+var astar_grid
 
 func _ready():
 	level_size = level_size/cell_size
@@ -17,6 +18,7 @@ func _ready():
 	rng.randomize()
 	
 	generate_level()
+	generate_astar_grid()
 
 func generate_level():
 	var walker_steps = 1024
@@ -27,3 +29,22 @@ func generate_level():
 	
 	for cell in map:
 		walls.erase_cell(cell)
+
+func generate_astar_grid():
+	astar_grid = AStarGrid2D.new()
+	astar_grid.region = walls.get_used_rect()
+	astar_grid.cell_size = Vector2i(cell_size, cell_size)
+	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	astar_grid.update()
+	
+	var region_size = astar_grid.region.size
+	var region_position = astar_grid.region.position
+	
+	for x in region_size.x:
+		for y in region_size.y:
+			var tile_position = Vector2i(x + region_position.x, y + region_position.y)
+			
+			var tile_data = walls.get_cell_tile_data(tile_position)
+			
+			if tile_data != null:
+				astar_grid.set_point_solid(tile_position)
