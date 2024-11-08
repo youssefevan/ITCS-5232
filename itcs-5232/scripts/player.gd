00447c93f7ca72edc1193095b531d0a851f84100
Evan <evanyoussef8@gmail.com>
2024-11-08 10:10:26 -0500

    restart
    - 3d top down 8 dir movement
    - 3d gridmap
    - lighting
    - temp models

--- itcs-5232/scripts/player.gd
+++ itcs-5232/scripts/player.gd
@@ -1,118 +1,20 @@
-extends Area2D
-class_name Player
+extends CharacterBody3D
 
-signal inventory_mode(mode : bool)
+const SPEED = 7.0
+const ACCEL = 10.0
 
-@onready var ray = $PhysicsRay
-@onready var attack_handler = $AttackHandler
-
-@export var attack_scene : PackedScene
-
-var last_movement_direction
-
-var colors = {
-	"fire": Color.RED,
-	"ice": Color.CYAN,
-	"magic": Color.MAGENTA,
-	"necrotic": Color.GREEN,
-}
-
-var tile_size = 8
-var animation_speed = 6
-var moving = false
-var attacking = false
-
-var inputs = {"right": Vector2.RIGHT,
-			"left": Vector2.LEFT,
-			"up": Vector2.UP,
-			"down": Vector2.DOWN}
-
-var active_inputs = []
-var in_inventory := false
+var input_dir : Vector3
 
 func _physics_process(delta):
-	if !attacking and !in_inventory:
-		handle_movement()
-	elif attacking and !in_inventory:
-		handle_aiming()
-
-func _unhandled_input(event: InputEvent) -> void:
-	if !in_inventory:
-		if Input.is_action_just_pressed("attack"):
-			if !attacking:
-				enter_attack()
-			else:
-				exit_attack()
-		
-		if Input.is_action_just_pressed("attack_cancel"):
-			if attacking:
-				exit_attack()
+	input_dir.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_dir.z = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
-	if Input.is_action_just_pressed("inventory_toggle"):
-		in_inventory = !in_inventory
-		active_inputs = []
-		emit_signal("inventory_mode", in_inventory)
-
-func handle_movement():
-	for dir in inputs.keys():
-		if Input.is_action_pressed(dir):
-			if dir in active_inputs:
-				pass
-			else:
-				active_inputs.append(dir)
-		elif Input.is_action_just_released(dir):
-			if dir in active_inputs:
-				active_inputs.erase(dir)
+	input_dir = input_dir.normalized()
 	
-	if len(active_inputs) > 0 and !moving:
-		move(active_inputs.back())
-
-func handle_aiming():
-	for dir in inputs.keys():
-		if Input.is_action_just_pressed(dir):
-			match dir:
-				"up":
-					attack_handler.rotation_degrees = 0
-				"down":
-					attack_handler.rotation_degrees = 180
-				"left":
-					attack_handler.rotation_degrees = -90
-				"right":
-					attack_handler.rotation_degrees = 90
-
-func move(dir):
-	ray.target_position = inputs[dir] * tile_size
+	velocity.x = lerpf(velocity.x, input_dir.x * SPEED, ACCEL * delta)
+	velocity.z = lerpf(velocity.z, input_dir.z * SPEED, ACCEL * delta)
 	
-	ray.force_raycast_update()
-	if !ray.is_colliding():
-		movement_tween(dir)
-		last_movement_direction = dir
-	elif ray.get_collider() is TileMap:# or ray.get_collider() is Enemy:
-		pass
-
-func movement_tween(dir):
-	var tween = create_tween()
-	tween.tween_property(self, "position", position + inputs[dir] * tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
-	moving = true
-	await tween.finished
-	moving = false
-
-func enter_attack():
-	attacking = true
-	active_inputs = []
+	var look_dir = Vector2(-velocity.x, velocity.z).angle() + PI/2
+	$Mesh.rotation.y = lerp_angle($Mesh.rotation.y, look_dir, ACCEL * delta)
 	
-	var attack = attack_scene.instantiate()
-	attack.type = attack.types.ICE
-	attack.color = colors["ice"]
-	attack_handler.add_child(attack)
-	attack.position.y -= tile_size
-
-func exit_attack():
-	attacking = false
-	for child in attack_handler.get_children():
-		child.call_deferred("free")
-
-func _on_fog_radius_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	if body is TileMapLayer:
-		var cell_pos = body.get_coords_for_body_rid(body_rid)
-		body.erase_cell(cell_pos)
+	move_and_slide()
