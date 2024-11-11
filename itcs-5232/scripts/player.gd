7a6c47df160183e4eb2049dfc174125ad887ec32
Evan <evanyoussef8@gmail.com>
2024-11-11 16:06:14 -0500

    Rework again

--- itcs-5232/scripts/player.gd
+++ itcs-5232/scripts/player.gd
@@ -1,20 +1,34 @@
 extends CharacterBody3D
 
-const SPEED = 7.0
-const ACCEL = 10.0
+var ray_origin = Vector3()
+var ray_target = Vector3()
+
+const MAX_SPEED = 400
 
 var input_dir : Vector3
 
 func _physics_process(delta):
+	handle_aim()
+	
 	input_dir.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input_dir.z = Input.get_action_strength("down") - Input.get_action_strength("up")
+	input_dir.y = 0
 	
-	input_dir = input_dir.normalized()
-	
-	velocity.x = lerpf(velocity.x, input_dir.x * SPEED, ACCEL * delta)
-	velocity.z = lerpf(velocity.z, input_dir.z * SPEED, ACCEL * delta)
-	
-	var look_dir = Vector2(-velocity.x, velocity.z).angle() + PI/2
-	$Mesh.rotation.y = lerp_angle($Mesh.rotation.y, look_dir, ACCEL * delta)
+	velocity = MAX_SPEED * input_dir * delta
 	
 	move_and_slide()
+
+func handle_aim():
+	var mouse_pos = get_viewport().get_mouse_position()
+	ray_origin = $"../Camera".project_ray_origin(mouse_pos)
+	ray_target = ray_origin + $"../Camera".project_ray_normal(mouse_pos) * 2000
+	
+	var space_state = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_target)
+	query.collide_with_areas = true
+	var intersection = space_state.intersect_ray(query)
+	
+	if not intersection.is_empty():
+		var pos = intersection.position
+		var look_to = Vector3(pos.x, 0, pos.z)
+		$Model.look_at(look_to, Vector3.UP)
