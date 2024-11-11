9f0292d1ee24bfa587b3fa5321b7019da3fd7535
Evan <evanyoussef8@gmail.com>
2024-11-11 17:01:01 -0500

    Lighting, temp models

--- itcs-5232/scripts/player.gd
+++ itcs-5232/scripts/player.gd
@@ -20,15 +20,21 @@ func _physics_process(delta):
 
 func handle_aim():
 	var mouse_pos = get_viewport().get_mouse_position()
-	ray_origin = $"../Camera".project_ray_origin(mouse_pos)
-	ray_target = ray_origin + $"../Camera".project_ray_normal(mouse_pos) * 2000
+	ray_origin = $Camera.project_ray_origin(mouse_pos)
+	ray_target = ray_origin + $Camera.project_ray_normal(mouse_pos) * 2000
 	
 	var space_state = get_world_3d().direct_space_state
 	var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_target)
+	query.collision_mask = -2147483648
 	query.collide_with_areas = true
 	var intersection = space_state.intersect_ray(query)
 	
 	if not intersection.is_empty():
 		var pos = intersection.position
-		var look_to = Vector3(pos.x, 0, pos.z)
+		var look_to = Vector3(pos.x, position.y, pos.z)
+		
 		$Model.look_at(look_to, Vector3.UP)
+		
+		# lock x and z axis
+		$Model.rotation.x = 0
+		$Model.rotation.z = 0
